67ee24dfa34269cb8e9cd161308d6b839dc08535
Zack Hudgins <zackhudg@gmail.com>
2023-09-02 17:15:12 -0400

    Got a basic implementation.

--- FF.py
+++ FF.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import operator
 
 # ppr = pd.read_csv(r'FantasyPros_2023_Overall_ADP_Rankings_PPR.csv')
 # halfppr = pd.read_csv(r'FantasyPros_2023_Overall_ADP_Rankings_HalfPPR.csv')
@@ -9,38 +10,52 @@ league_size = 10 #input("How many teams in the league?")
 league_scoring = 'PPR' #input("What scoring method is used? (PPR, HalfPPR, STD)")
 draft_number = 1 #input("What is your pick number?")
 
-teams = {}
+teams = list()
 
 # OR: import league settings
 
 filePath = 'FantasyPros_2023_Overall_ADP_Rankings_'+league_scoring+'.csv'
 df = pd.read_csv(filePath)
-for i in range(1, league_size):
-    draft_number = i
+for i in range(1, league_size+1):
+    for bye_week in range(1, 17): # Week number
 
-    for j in range(10, 14): # Week number
+        team = pd.DataFrame()
+        draft_number = i
         next_pick = draft_number
 
-        current_df = df.loc[df['Bye'] == j]
-        team = pd.DataFrame()
-        score = 0
+        current_df = df.loc[df['Bye'] == bye_week]
+        score = 0.0
 
-        for k in range(0, 3): # 8 Starters
+        # Potential Scenarios:
+        # 1: Everyone drafts at ADP (Expected)
+        # 2: I can reach a few picks above ADP (Upper)
+        # 3: I get sniped every time (Lower)
+        # 4: Feed in mocks?
+
+        for round_number in range(0, 10): # 8 Starters
             pick = current_df[current_df['Rank'] >= next_pick].iloc[:1]
+            pick['OverallPick'] = next_pick
             current_df = current_df[current_df['Rank'] >= next_pick].iloc[1:]
 
             team = pd.concat([team, pick])
-            score += pick['Rank']
+            score += pick['AVG'].values
 
-            next_draft_number = league_size - draft_number
-            next_pick += 2 * (next_draft_number) + 1
+            next_draft_number = league_size - draft_number + 1
+            next_pick += (2 * next_draft_number) - 1
             draft_number = next_draft_number
-        
-        teams[j] =  (score, team)
 
-for team in teams:
-    print('------------', team, '------------')
-    print(teams[team][0])
-    print(teams[team][1])
+        if len(score) != 0:
+            teams.append((i, bye_week, score[0], team)) # i = original draft_number
+
+teams.sort(key=lambda v : v[2])
+for i in range(0, len(teams)):
+    team = teams[i]
+    print('------------------------')
+    print('Draft Number:', team[0])
+    print('Bye Week:', team[1])
+    print('Score:', team[2])
+    if i < 10:
+        print('Team:\n', team[3])
+
 
 
